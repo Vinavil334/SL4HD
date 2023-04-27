e0266c0d77d068bf80a1836842cd363df0549485
Marco Poggi <84519000+BertrandPouget@users.noreply.github.com>
2023-04-27 17:57:13 +0200

    data10

---
+++ create_more_classes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def get_classes(files):
+
+    #########################################################
+    ## INPUT:                                              ##
+    ## - files: string containing the name of a .csv table ##
+    ##                                                     ##
+    ## OUTPUT:                                             ##
+    ## - df: dataframe keeping only the 'classes'          ##
+    ##        attribute                                    ##
+    #########################################################
+
+    df = pd.read_csv(files, delimiter=';')
+
+    df.sort_values(by=['folder'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.drop(columns = ['Unnamed: 0','folder'], inplace=True)
+
+    print(df)
+    print(df.columns)
+
+    return df
+
+df = get_classes('files.csv')
+print('\n\n',df)
+
+df.to_csv('more_classes.csv', index=False)
